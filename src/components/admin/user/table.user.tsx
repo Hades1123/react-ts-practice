3cc9b8e5b8a3f4c1a6dc3dd79bdea6e69b79ee05
Hades1123 <dp1.1a7.si@gmail.com>
2025-07-08 13:54:53 +0700

    reorder some code

--- src/components/admin/user/table.user.tsx
+++ src/components/admin/user/table.user.tsx
@@ -1,55 +1,14 @@
 import { getUserAPI } from '@/services/api';
 import { DeleteOutlined, EditOutlined, PlusOutlined } from '@ant-design/icons';
 import type { ActionType, ProColumns } from '@ant-design/pro-components';
-import { ProTable, TableDropdown } from '@ant-design/pro-components';
-import { Button, Space, Tag } from 'antd';
+import { ProTable } from '@ant-design/pro-components';
+import { Button, Space } from 'antd';
 import { useRef, useState } from 'react';
 
-
-const columns: ProColumns<IUserTable>[] = [
-    {
-        dataIndex: 'index',
-        valueType: 'indexBorder',
-        width: 48,
-    },
-    {
-        title: 'id',
-        dataIndex: '_id',
-        render: (_, record) => <a>{record._id}</a>,
-        search: false,
-    },
-    {
-        title: 'Full name',
-        dataIndex: 'fullName',
-    },
-    {
-        title: 'Email',
-        dataIndex: 'email',
-        copyable: true,
-    },
-    {
-        title: 'Created at',
-        dataIndex: 'createdAt',
-        valueType: 'date',
-    },
-    {
-
-        title: 'Action',
-        key: 'action',
-        render: (_, record) => (
-            <Space size="middle">
-                <EditOutlined style={{ color: 'orange', cursor: 'pointer' }} />
-                <DeleteOutlined style={{ color: 'red', cursor: 'pointer' }} />
-            </Space>
-        ),
-        search: false,
-    }
-];
 
 const TableUser = () => {
     const [page, setPage] = useState<number>(1);
     const [pageSize, setPageSize] = useState<number>(5);
-    const [DataTable, setDataTable] = useState<IUserTable[] | null>(null);
 
     const onChangePage = (page: number) => {
         setPage(page)
@@ -58,6 +17,51 @@ const TableUser = () => {
     const onPageSizeChange = (page: number, pageSize: number) => {
         setPageSize(pageSize)
     }
+
+    const columns: ProColumns<IUserTable>[] = [
+        {
+            title: "STT",
+            dataIndex: 'index',
+            valueType: 'indexBorder',
+            width: 48,
+            render: (_, record, index) => {
+                return <span>{index + 1 + (page - 1) * pageSize}</span>
+            }
+        },
+        {
+            title: 'id',
+            dataIndex: '_id',
+            render: (_, record) => <a>{record._id}</a>,
+            search: false,
+        },
+        {
+            title: 'Full name',
+            dataIndex: 'fullName',
+        },
+        {
+            title: 'Email',
+            dataIndex: 'email',
+            copyable: true,
+        },
+        {
+            title: 'Created at',
+            dataIndex: 'createdAt',
+            valueType: 'date',
+        },
+        {
+
+            title: 'Action',
+            key: 'action',
+            render: (_, record) => (
+                <Space size="middle">
+                    <EditOutlined style={{ color: 'orange', cursor: 'pointer' }} />
+                    <DeleteOutlined style={{ color: 'red', cursor: 'pointer' }} />
+                </Space>
+            ),
+            search: false,
+        }
+    ];
+
     const actionRef = useRef<ActionType>();
     return (
         <>
@@ -66,12 +70,11 @@ const TableUser = () => {
                 actionRef={actionRef}
                 cardBordered
                 request={async (params, sort, filter) => {
-                    // console.log(sort, filter);
-                    console.log('render')
+                    // console.log('render')
                     const resultAPI = await getUserAPI(page, pageSize);
                     return {
                         data: resultAPI.data?.result,
-                        "page": 1,
+                        "page": page,
                         "success": true,
                         "total": resultAPI.data?.meta.total,
                     }
@@ -84,8 +87,8 @@ const TableUser = () => {
                 pagination={{
                     pageSize: pageSize,
                     current: page,
-                    onChange: onChangePage,
                     showSizeChanger: true,
+                    onChange: onChangePage,
                     onShowSizeChange: onPageSizeChange,
                 }}
                 dateFormatter="string"
@@ -101,7 +104,6 @@ const TableUser = () => {
                     >
                         Add new
                     </Button>
-
                 ]}
             />
         </>
